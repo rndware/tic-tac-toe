edf445ce273f130c49abc3e0042e0366275a1a8b
Adam Smith <abs2g08@gmail.com>
2022-08-29 12:25:08 +0100

    Resolve type issue

--- src/const/gridData.ts
+++ src/const/gridData.ts
@@ -1,20 +1,20 @@
-import { GridItem } from "../types/game";
+import { GridIndex } from "../types/game";
 
 export const gridSize = 3;
 
-export const rowIndexes: GridItem[][] = [
+export const rowIndexes: GridIndex[][] = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
 ];
 
-export const columnIndexes: GridItem[][] = [
+export const columnIndexes: GridIndex[][] = [
   [0, 3, 6],
   [1, 4, 7],
   [2, 5, 8],
 ];
 
-export const diagnalIndexes: GridItem[][] = [
+export const diagnalIndexes: GridIndex[][] = [
   [0, 4, 8],
   [2, 4, 6],
 ];
