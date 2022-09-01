b8f4280d3c80a26b0158cc3092bc06b518233fd9
Adam Smith <abs2g08@gmail.com>
2022-09-01 12:56:20 +0100

    Refactor component

--- src/pages/setup/Setup.tsx
+++ src/pages/setup/Setup.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 
-export default function Setup() {
-  return (
-    <div className="SetupPage">
-      <Typography variant="h3" component="h3">
-        Game Setup
-      </Typography>
-      <div>TO-DO: add user form to set: Name, age, player colour etc.</div>
-      <div>
-        <Link to="/game">Start Game</Link>
-      </div>
+const Setup = () => (
+  <div className="SetupPage">
+    <Typography variant="h3" component="h3">
+      Game Setup
+    </Typography>
+    <div>TO-DO: add user form to set: Name, age, player colour etc.</div>
+    <div>
+      <Link to="/game">Start Game</Link>
     </div>
-  );
-}
+  </div>
+);
+
+export default Setup;
