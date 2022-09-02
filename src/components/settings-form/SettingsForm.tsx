2ed3b2a0f31a51a988477f03df383dc9da5aabec
Adam Smith <abs2g08@gmail.com>
2022-09-02 13:32:13 +0100

    Refactor settings form

--- src/components/settings-form/SettingsForm.tsx
+++ src/components/settings-form/SettingsForm.tsx
@@ -13,28 +13,62 @@ import { Difficulty, Lang } from "../../types/game";
 
 import styles from "./SettingsForm.module.scss";
 
+interface enumMap {
+  [key: string]: string;
+}
+
+type Options = { [key: string]: string };
+
 interface SettingsFormProps {
   copy: I18nCopy;
   difficulty: Difficulty;
   onDifficultyChange: (e: any) => void;
 }
 
-// TO-DO: combine renderDifficultyOptions and renderLangOptions into one generic function
-const renderDifficultyOptions = (options: { [key: string]: string }) =>
-  (Object.keys(Difficulty) as Array<keyof typeof Difficulty>).map(
-    (value: string) => (
-      <MenuItem key={`difficulty-settings-item-${value}`} value={value}>
-        {options[value.toLowerCase()]}
+const renderSelectOptions = (
+  key: string,
+  enumOptions: enumMap,
+  options: Options
+) =>
+  (Object.keys(enumOptions) as Array<keyof typeof enumOptions>).map(
+    (value: string | number) => (
+      <MenuItem key={`${key}-options-item-${value}`} value={value}>
+        {options[value.toString().toLowerCase()]}
       </MenuItem>
     )
   );
 
-const renderLangOptions = (options: { [key: string]: string }) =>
-  (Object.keys(Lang) as Array<keyof typeof Lang>).map((value: string) => (
-    <MenuItem key={`lang-settings-item-${value}`} value={value}>
-      {options[value.toLowerCase()]}
-    </MenuItem>
+interface FormControlData {
+  key: string;
+  copy: I18nCopy;
+  value: string;
+  enum: enumMap;
+  onChange: (e: any) => void;
+  options: Options;
+}
+
+const renderFormControls = (formControls: FormControlData[]) => {
+  return formControls.map((item: FormControlData) => (
+    <div
+      key={`form-control-item-${item.key}`}
+      className={styles.SettingsForm__formControl}
+    >
+      <FormControl>
+        <InputLabel id={`${item.key}-label`}>{item.copy.label}</InputLabel>
+        <Select
+          className={styles.Select}
+          labelId={`${item.key}-label`}
+          id={`${item.key}-select`}
+          value={item.value}
+          label={item.copy.label}
+          onChange={item.onChange}
+        >
+          {renderSelectOptions(item.key, item.enum, item.options)}
+        </Select>
+      </FormControl>
+    </div>
   ));
+};
 
 const SettingsForm = (props: SettingsFormProps) => {
   const { i18n } = useTranslation();
@@ -46,37 +80,28 @@ const SettingsForm = (props: SettingsFormProps) => {
     i18n.changeLanguage(lang);
   };
 
-  // TO-DO: create a generic FormControl rendering service to avoid duplicate code
+  const formControls: FormControlData[] = [
+    {
+      key: "difficulty",
+      copy: props.copy.difficulty,
+      value: props.difficulty,
+      enum: Difficulty,
+      onChange: props.onDifficultyChange,
+      options: props.copy.difficulty.options,
+    },
+    {
+      key: "lang",
+      copy: props.copy.lang,
+      value: language,
+      enum: Lang,
+      onChange: handleLangChange,
+      options: props.copy.lang.options,
+    },
+  ];
+
   return (
     <form className={styles.SettingsForm} noValidate autoComplete="off">
-      <FormControl className={styles.FormControl}>
-        <InputLabel id="difficulty-setting-label">
-          {props.copy.difficulty.label}
-        </InputLabel>
-        <Select
-          className={styles.Select}
-          labelId="difficulty-setting-label"
-          id="difficulty-setting-select"
-          value={props.difficulty}
-          label="difficulty"
-          onChange={props.onDifficultyChange}
-        >
-          {renderDifficultyOptions(props.copy.difficulty.options)}
-        </Select>
-      </FormControl>
-      <FormControl className={styles.FormControl}>
-        <InputLabel id="lang-setting-label">{props.copy.lang.label}</InputLabel>
-        <Select
-          className={styles.Select}
-          labelId="lang-setting-label"
-          id="lang-setting-select"
-          value={language}
-          label="lang"
-          onChange={handleLangChange}
-        >
-          {renderLangOptions(props.copy.lang.options)}
-        </Select>
-      </FormControl>
+      {renderFormControls(formControls)}
       <div className={styles.SettingsForm__controls}>
         <Button
           type="submit"
